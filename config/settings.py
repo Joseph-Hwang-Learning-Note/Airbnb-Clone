260d372d930f5ba8cd367abc7e619ddeac2bbef3
01Joseph-Hwang10 <72839543+01Joseph-Hwang10@users.noreply.github.com>
2020-12-28 20:08:24 +0900

    having a hard times

--- config/settings.py
+++ config/settings.py
@@ -179,9 +179,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 
 AUTH_USER_MODEL = "users.User"
 
@@ -191,7 +191,7 @@ MEDIA_URL = "/media/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Email Configuration
